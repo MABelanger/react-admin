2ca8d38a5fca6243ddb394e6e54f8263b80544ed
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2016-03-12 01:58:09 -0500

    async call

--- src/js/components/commons/forms/Form.js
+++ src/js/components/commons/forms/Form.js
@@ -1,19 +1,34 @@
-import React from "react";
-
-import TextInput from "./TextInput";
+import React                      from "react";
+import TextInput                  from "./TextInput";
+import Request                    from "superagent";
 
 export default class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       courses: [],
+      course: {}
     };
   }
 
-  changeValue(name, value) {
-    console.log(name, value)
-    this.state.courses[name] = value;
-    this.setState({ courses:  this.state.courses });
+  componentWillMount() {
+    console.log('componentWillMount');
+    var URL = 'http://localhost:3000/api/courses';
+    //var URL = 'http://www.omdbapi.com/?s=toto';
+    Request
+    .get(URL, function(err, res){
+      console.log(res.body[0])
+      this.setState({
+        courses: res.body,
+        course: res.body[0]
+      });
+    }.bind(this));
+
+  }
+
+  changeName(name, value) {
+    this.state.course.name = value;
+    this.setState({ course:  this.state.course });
   }
 
   render() {
@@ -22,8 +37,8 @@ export default class Form extends React.Component {
         <TextInput
           name="name"
           label="Nom"
-          value={this.state.courses.name}
-          changeValue={this.changeValue.bind(this)}
+          value={this.state.course.name}
+          changeValue={this.changeName.bind(this)}
           />
       </div>
     );
