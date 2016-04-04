a21509c22a95912f475c0f768c260d79b0338882
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2016-04-04 01:51:17 -0400

    fix date

--- src/js/components/commons/PickerCommon.js
+++ src/js/components/commons/PickerCommon.js
@@ -21,10 +21,10 @@ export default class PickerCommon extends React.Component {
     let defaultText = null;
     let inputFormat = null;
     let myDate = this.props.date;
-    //console.log('-', moment().startOf('day').utcOffset("-04:00").toISOString());
+
+    // if no date, by default is the today at 00:00h
     if(! myDate){
       myDate = String(moment().utcOffset("+00:00").startOf('day').toISOString());//"2016-03-18T20:28:00.000Z"
-      console.log('myDate', myDate)
     }
 
     if (mode == 'date') {
@@ -36,20 +36,17 @@ export default class PickerCommon extends React.Component {
       inputFormat = INPUT_FORMAT_TIME;
     }
 
-    //return this.renderDateTime(mode, defaultText, inputFormat, date);
-      return (
-        <div>
-        <DateTimeField
-          ref={mode + "Field"}
-          mode={mode}
-          defaultText="allo"
-          dateTime={String(myDate)}
-          format={FORMAT}
-          inputFormat={inputFormat}
-          onChange={this.handleChange}
-        />
-      </div>
-      );
+    return (
+      <DateTimeField
+        ref={mode + "Field"}
+        mode={mode}
+        defaultText={defaultText}
+        dateTime={String(myDate)}
+        format={FORMAT}
+        inputFormat={inputFormat}
+        onChange={this.handleChange}
+      />
+    );
   }
 
   render() {
